f98713f9cbbd1410ca300b826a1531b553e47b8c
CS70 Cricket <syasunaga@cs.hmc.edu>
2018-06-05 12:31:50 +1000

    made it clean

--- EEG/Matlab/spm2eeglab.m
+++ EEG/Matlab/spm2eeglab.m
@@ -11,16 +11,18 @@ clear all
 % Edit here (if needed, including Constant)
 % Directory to the spm files
 % *Do not put anything except the data 
-spm_dir = '/Users/macbookpro/Documents/Tsuchiya_Lab_Data/Probes/spm';
+spm_dir = '/Volumes/SHard/Trials/spm';
 
 % Look at convert_location_mat2eeglab if you don't have one
 location_file_dir = ...
-    '/Users/macbookpro/Dropbox/College/TsuchiyaLab/wandercatch/EEG/chan_loc.xyz';
+    '/Users/macbookpro/Dropbox/College/TsuchiyaLab/wandercatch/EEG/Matlab/chan_loc.xyz';
 
-saving_path = '/Users/macbookpro/Documents/Tsuchiya_Lab_Data/Probes/eeglab';
-% Directory where you want to save the figure
-% saving_dir = '/Users/macbookpro/Dropbox/College/TsuchiyaLab/Plots';
 
+% Path to save the eeglab set
+saving_path = '/Volumes/SHard/Trials/eeglab';
+
+% file, util. Has to have util.m in it. 
+util_dir     = '/Users/macbookpro/Dropbox/College/TsuchiyaLab/wandercatch/';
 
 %%%%%%%%%%%%
 % Constant %
@@ -32,51 +34,47 @@ Fs = 500;
 % End of Editting %
 %%%%%%%%%%%%%%%%%%%
 
+%%%%%%%%%%%%%%
+% Dependency %
+%%%%%%%%%%%%%%
+% util
+addpath(util_dir)
+%eeglab
+eeglab
+
 %%
 %%%%%%%%%%
 % Script %
 %%%%%%%%%%
 
 
-files = dir(spm_dir);
-files=files(~ismember({files.name},{'.','..'}));
+
+files= util('getBehaviorFiles',spm_dir); % It's not behavior, but it's same .mat file.
 
 cd(spm_dir)
 
-eeglab
 
 for i = 1:length(files)
-    if isMat((files(i).name))
-        file_name = files(i).name;
-        name_wo_mat = file_name(1:end-4); 
-        fprintf('Processing %s',char(file_name))
-        load(files(i).name)
-        D.data.fname = sprintf('%s%c%s%s',spm_dir,'/',name_wo_mat,'.dat');
-        data = D.data(1:NUM_CHANNELS,:,:);
-        EEG = pop_importdata('dataformat','array','nbchan',NUM_CHANNELS,'data','data',...
-            'setname',name_wo_mat,'srate',Fs,'pnts',0,'xmin',0,'chanlocs',...
-            location_file_dir);
-        
-        % Fix the radius value 
-        % Please let me know if you find better way to do this
-        
-        for c = 1:NUM_CHANNELS
-            x = EEG.chanlocs(c).X;
-            y = EEG.chanlocs(c).Y;
-            EEG.chanlocs(c).radius = euclid_dist(x,y)*0.50/0.660; %TODO
-        end
-        
-        EEG = eeg_checkset( EEG );
+    [~,name,ext] = fileparts(files{i});
+    fprintf('Processing %s',name)
+    load(files{i})
+    D.data.fname = sprintf('%s%c%s%s',spm_dir,'/',name,'.dat');
+    data = D.data(1:NUM_CHANNELS,:,:);
+    EEG = pop_importdata('dataformat','array','nbchan',NUM_CHANNELS,'data','data',...
+        'setname',name,'srate',Fs,'pnts',0,'xmin',0,'chanlocs',...
+        location_file_dir);
 
-        
-        EEG = pop_saveset( EEG, 'filename',name_wo_mat,'filepath',saving_path); % Save the data
+    % Fix the radius value 
+    % Please let me know if you find better way to do this
+
+    for c = 1:NUM_CHANNELS
+        x = EEG.chanlocs(c).X;
+        y = EEG.chanlocs(c).Y;
+        EEG.chanlocs(c).radius = euclid_dist(x,y)*0.50/0.660; %TODO
     end
-end
 
-
-function bool = isMat(str)
-    % returns if str ends with .mat (if it contains .mat to be precise)
-    bool =not(isempty(strfind(str,'.mat')));
+    EEG = eeg_checkset( EEG );
+    EEG = pop_saveset( EEG, 'filename',name,'filepath',saving_path); % Save the data
 end
 
     
